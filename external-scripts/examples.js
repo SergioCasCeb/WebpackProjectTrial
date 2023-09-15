f2835ea24fee8924c14dbf9631ba829422485224
Sergio Eduardo Castro Ceballos <72516762+SergioCasCeb@users.noreply.github.com>
2023-09-15 16:46:30 +0200

    updated the examples.js file

--- external-scripts/examples.js
+++ external-scripts/examples.js
@@ -1,59 +1,78 @@
 /**
- * @file The `examples.js` contains generates a json file
- * with all the links to the respective raw links in the 
+ * @file The `examples.js` generates a json file
+ * with all the links, title and descriptions of the TD and TM examples in the 
  * main github repository utilizing the examples folder
  */
 
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 //path to where all the td/tm examples
 const initialPath = "./examples";
 //path to the get the raw files from the github
 const rawFilePath = "https://raw.githubusercontent.com/eclipse-thingweb/playground/master/examples"
-let files = fs.readdirSync(initialPath);
 
-let examplesPaths = {}
+async function getExamples(){
+    const files = await fs.readdir(initialPath)
 
-files.forEach(file => {
-    //initializing the object by creating a td and a tm object
-    examplesPaths[file] = {}
-    
-    //getting all the cattegories from the td/tm files
-    let categories = fs.readdirSync(initialPath+"/"+file)
+    const examplesPaths = {}
 
-    //sorting them by the first number in their name
-    categories.sort((a, b) => a.localeCompare(b, undefined, { numeric: true }))
-    categories.forEach(category => {
+    for(const file of files){
+        examplesPaths[file] = {}
 
-        let examples = fs.readdirSync(initialPath+"/"+file+"/"+category)
-        
-        //for each category, create a description with the path to the readMe file, and an examples object to store all the respective exampele
-        examplesPaths[file][category] = {
-            "description": "",
-            "examples": {}
-        }
+        const categories = await fs.readdir(path.join(initialPath, file))
 
-        examples.forEach(example => {
-            //for each category, get the readMe file to be used as the description
-            if(path.extname(example) == ".txt"){
-                examplesPaths[file][category]["description"] = `${rawFilePath}/${file}/${category}/${example}`
+        //sorting them by the first number in their name
+        categories.sort((a, b) => a.localeCompare(b, undefined, { numeric: true }))
+
+        for(const category of categories){
+
+            const examples = await fs.readdir(path.join(initialPath, file, category))
+
+            //for each category, create a description with the path to the readMe file, and an examples object to store all the respective exampele
+            examplesPaths[file][category] = {
+                "description": "",
+                "examples": {}
             }
 
-            //for each category get the .jsonld files to use as the examples
-            if(path.extname(example) == ".jsonld"){
-                examplesPaths[file][category]["examples"][example] = {
-                    "path": `${rawFilePath}/${file}/${category}/${example}`
+            for(const example of examples){
+                if(path.extname(example) == ".txt"){
+                    try {
+                        const categoryDescription = await fs.readFile(path.join(initialPath, file, category, example), 'utf8')
+                        examplesPaths[file][category]["description"] = categoryDescription
+                    } catch (err) {
+                        console.error("Failed to read file: ", err);
+                    }
+                }
+
+                if(path.extname(example) == ".jsonld"){
+                    const exampleData = JSON.parse(await fs.readFile(path.join(initialPath, file, category, example), 'utf8'))
+                    const exampleTitle = exampleData["$title"]
+                    const exampleDescription = exampleData["$description"]
+                    examplesPaths[file][category]["examples"][example] = {
+                        "title": exampleTitle,
+                        "description": exampleDescription,
+                        "path": `${rawFilePath}/${file}/${category}/${example}`
+                    }
                 }
             }
-        })
-    })
-})
-
-//Creating the json file to be used by the application
-fs.writeFile("./src/examples/examples-paths.json", JSON.stringify(examplesPaths, null, 2), 'utf-8', (err) => {
-    if(err){
-        console.log(err)
-    }else{
-        console.log("File created succesfully");
+        }
     }
-})
+
+    return examplesPaths
+}
+
+async function writeExamplesToFile(){
+    const examplesData = await getExamples()
+    
+    try {
+        const jsonData = JSON.stringify(examplesData, null, 2)
+        const filePath = "./src/examples/examples-paths.json"
+        await fs.writeFile(filePath, jsonData, 'utf-8')
+        console.log("File created succesfully");
+        
+    } catch (err) {
+        console.error("Failed to wirte JSON file: ", err);
+    }
+}
+
+writeExamplesToFile()
